3f15250971c0670e0d8f3dcda05e78d1766c8a62
Adam Zamlynny <azamlynny@hotmail.com>
2018-12-26 22:14:48 -0500

    Added a second button. A switchboard was made by Adam and the code was tested. It works great!

--- Switchboard/Switchboard.ino
+++ Switchboard/Switchboard.ino
@@ -1,25 +1,43 @@
 #include <Mouse.h>
+#include <Keyboard.h>
 
-int buttonPin = 9;     // the number of the pushbutton pin
-int buttonState = 0;         // for reading the button
-boolean pressed = false;
+int buttonAPin = 9;     // the number of the pushbutton pin
+int buttonBPin = 8;
+int buttonAState = 0;         // for reading the button
+int buttonBState = 0;
+
+boolean pressedA = false;
+boolean pressedB = false;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(buttonPin, INPUT_PULLUP); // tells the arduino board that the thing in place 2 is input. Either it can have power and have a value of 1 or 0.
+  pinMode(buttonAPin, INPUT_PULLUP); // tells the arduino board that the thing in place 2 is input. Either it can have power and have a value of 1 or 0.
+  pinMode(buttonBPin, INPUT_PULLUP);
+
   Mouse.begin();
+  Keyboard.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW && pressed == false) {
+  buttonAState = digitalRead(buttonAPin);
+  buttonBState = digitalRead(buttonBPin);
+  if (buttonAState == LOW && pressedA == false) { // Power is low because when the button is pressed the power goes to ground
     Mouse.press(MOUSE_LEFT);
-    pressed = true;
+    pressedA = true;
   }
-  else if (buttonState == HIGH && pressed == true){
+  else if (buttonAState == HIGH && pressedA == true){
     Mouse.release(MOUSE_LEFT);
-    pressed = false;
+    pressedA = false;
   }
 
+  if (buttonBState == LOW && pressedB == false) { // Power is low because when the button is pressed the power goes to ground
+    Keyboard.press(' ');
+    pressedB = true;
+  }
+  else if (buttonBState == HIGH && pressedB == true){
+    Keyboard.release(' ');
+    pressedB = false;
+  }
+  
 }
